869a8ac4bea9115cf8fb6a2dfae1c66b882bb782
amenson1983 <75535151+amenson1983@users.noreply.github.com>
2020-12-07 17:27:03 +0200

    Tasks for Chapter 2

--- Chapter 2 StockExchange.py
+++ Chapter 2 StockExchange.py
@@ -6,3 +6,7 @@ s_sum = float(42.75*s_stock) #Sold stock amount
 s_br_rew = 0.03*s_sum #Sold stock broker reward
 profit = (s_sum)-(b_sum+b_br_rew+s_br_rew)
 print("Bought stock quantity: ", b_stock, "\nSold stock quantity: ", s_stock, "\nBroker purchase reward: ", b_br_rew, "\nBroker sell reward: ", s_br_rew, "\nIncome amount: ", s_sum, "\nProfit amount: ", profit)
+if profit>0:
+    print("OK, Joe got profit in amount: ", profit)
+else: print("Oh no, loses consist: ", profit)
+
