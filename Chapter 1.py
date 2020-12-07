d0f8679b9f60ef5a59c8eab806e55151fa622238
amenson1983 <75535151+amenson1983@users.noreply.github.com>
2020-12-07 12:07:14 +0200

    Tasks for Chapter 1

---
+++ Chapter 1.py
@@ -0,0 +1,13 @@
+a = bin(11)
+b = bin(65)
+c = bin(100)
+d = bin(255)
+print(a,b,c,d)
+j = 0b1101
+k = 0b1000
+l = 0b101011
+print(j,k,l)
+
+
+
+
--- test_for_git_print.py
+++
@@ -1,3 +0,0 @@
-x = input("Hello, my friend, what`s your name?")
-print("Excellent! Good for you!")
-print("Hello, ", x)
