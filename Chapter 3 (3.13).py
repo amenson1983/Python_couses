80526d2de026f431d533308e4d28afd9f4fbc219
amenson1983 <75535151+amenson1983@users.noreply.github.com>
2020-12-08 10:01:09 +0200

    Tasks for Chapter 2

---
+++ Chapter 3 (3.13).py
@@ -0,0 +1,9 @@
+number = int(input("Пожалуйста, введите целое число: "))
+if number == 1:
+    print("One")
+elif number == 2:
+    print("Two")
+elif number == 3:
+    print("Three")
+else:
+    print("Unknown")
---
+++ Chapter 3 (3.7).py
@@ -0,0 +1,4 @@
+sales = float(input("Please put sales: "))
+if sales >= 10000:
+    comissionRate = 0.2
+print(comissionRate)
